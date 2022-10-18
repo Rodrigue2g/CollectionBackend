5931657889c552eed7bfef3a0be0d90883683bd0
Rodrigue de Guerre <99824035+Dequavious6@users.noreply.github.com>
2022-10-18 22:45:11 +0200

    import

---
+++ pipImport.py
@@ -0,0 +1,8 @@
+#from urllib import *
+import urllib
+import httpimport
+url = "https://raw.githubusercontent.com/Dequavious6/CollectionBackend/main"
+with httpimport.remote_repo(["testUL"], url):
+    from testUL import *
+with httpimport.remote_repo(["pipUtility"], url):
+    from pipUtility import *
